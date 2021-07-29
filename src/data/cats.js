0c4a97350d8591150f65c7a76f6211d8e8d2b8e3
fede <federico.caiello1994@gmail.com>
2021-07-29 17:32:48 +0200

    first commit

---
+++ src/data/cats.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'coll',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Hilly',
+    breed: 'mut',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Zus',
+    breed: 'afghan ',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    location: 'threed',
+    notes: 'super friendly'
+  },
+  {
+    name: 'ktie',
+    breed: 'golden ',
+    gender: 'female',
+    age: 2,
+    color: 'black',
+    weight: 44,
+    location: 'threed',
+    notes: 'super friendly'
+  }
+]
